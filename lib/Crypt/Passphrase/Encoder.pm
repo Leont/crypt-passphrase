2f97f332668e0c40b923830e73cb4ddb2885403d
Leon Timmermans <fawaka@gmail.com>
2022-10-29 11:54:19 +0200

    Never accept undefined hashes

--- lib/Crypt/Passphrase/Encoder.pm
+++ lib/Crypt/Passphrase/Encoder.pm
@@ -20,6 +20,7 @@ sub crypt_subtypes {
 my %cache;
 sub accepts_hash {
 	my ($self, $hash) = @_;
+	return if not defined $hash;
 	$cache{$self} ||= do {
 		my $string = join '|', $self->crypt_subtypes or return;
 		qr/ \A \$ (?: $string ) \$ /x;
