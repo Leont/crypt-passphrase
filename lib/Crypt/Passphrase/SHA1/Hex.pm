dce6239cc43246f7999e073e6900c45f3eee7f54
Leon Timmermans <fawaka@gmail.com>
2023-02-08 01:34:03 +0100

    Add Add Crypt::Passphrase::SHA1::Hex

---
+++ lib/Crypt/Passphrase/SHA1/Hex.pm
@@ -0,0 +1,41 @@
+package Crypt::Passphrase::SHA1::Hex;
+
+use parent 'Crypt::Passphrase::Validator';
+
+use Digest::SHA 'sha1_hex';
+
+sub new {
+	my $class = shift;
+	return bless {}, $class;
+}
+
+sub accepts_hash {
+	my ($self, $hash) = @_;
+	return $hash =~ / ^ [A-Fa-f0-9]{40} $/x;
+}
+
+sub verify_password {
+	my ($self, $password, $hash) = @_;
+	return sha1_hex($password) eq $hash;
+}
+
+1;
+
+# ABSTRACT: Validate against hexed MD5 hashes with Crypt::Passphrase
+
+=head1 DESCRIPTION
+
+This module implements a validator
+
+=method new()
+
+This creates a new SHA1 validator. It takes no arguments.
+
+=method accepts_hash($hash)
+
+This (heuristically) determines if we may be dealing with a hex encoded md5 sum.
+
+=method verify_hash($password, $hash)
+
+This determines if the password matches the hash when SHA1'ed.
+
